4d97d3451a1e80508b0d190bf9f2e0a68e033f18
Maks <mhromyk.edu@gmail.com>
2023-12-08 17:10:43 +0100

    initial empty project

---
+++ src/main/java/mh/springboot/restapi/openfeignconsumer/OpenfeignconsumerApplication.java
@@ -0,0 +1,13 @@
+package mh.springboot.restapi.openfeignconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenfeignconsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenfeignconsumerApplication.class, args);
+	}
+
+}
